e2e3c8bfadbd7c8f4060be31eba54d0e130c9c62
kimwonjun1 <monkeydmw@naver.com>
2024-05-05 08:29:20 +0900

    Update VerificationTokenService.java

--- v1/finance/service/VerificationTokenService.java
+++ v1/finance/service/VerificationTokenService.java
@@ -1,13 +1,16 @@
-package com.capstone.finance.service;
+package com.capstone.finance.Service;
 
-import com.capstone.finance.dao.MemberRepository;
-import com.capstone.finance.dao.VerificationTokenRepository;
-import com.capstone.finance.model.Member;
-import com.capstone.finance.model.VerificationToken;
+import com.capstone.finance.DAO.MemberRepository;
+import com.capstone.finance.DAO.VerificationTokenRepository;
+import com.capstone.finance.Entity.Member;
+import com.capstone.finance.Entity.VerificationToken;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Base64;
 import java.util.UUID;
 
 @Service
@@ -15,8 +18,6 @@ import java.util.UUID;
 public class VerificationTokenService {
 
     private final VerificationTokenRepository verificationTokenRepository;
-
-    //private final SmtpEmailService smtpEmailService;
 
     private final MemberRepository memberRepository;
 
@@ -30,7 +31,37 @@ public class VerificationTokenService {
         verificationToken.setExpiryDate(calculateExpiryDate());
         verificationTokenRepository.save(verificationToken);
 
-        //smtpEmailService.sendVerificationCode(member.getUsername(), token);
+    }
+
+    @Value("${jwt.secret}") // application.properties의 jwt.secret 값 가져오기
+    private String secretKey; // 토큰 서명에 사용되는 비밀키
+
+    public boolean validateToken(String token) {
+        try {
+            // Base64 디코딩하여 byte[]로 변환
+            byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+
+            Claims claims = Jwts.parserBuilder() // parserBuilder 사용
+                    .setSigningKey(keyBytes) // byte[] 형식의 키 사용
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // 토큰이 만료되었는지 확인
+            long expirationTime = claims.getExpiration().getTime();
+            long currentTime = System.currentTimeMillis();
+            return expirationTime > currentTime; // 만료되지 않았으면 true 반환
+
+        } catch (ExpiredJwtException e) {
+            // 토큰이 만료된 경우
+            return false;
+        } catch (JwtException e) {
+            // 서명 오류 또는 기타 JWT 관련 오류가 있는 경우
+            return false;
+        } catch (Exception e) {
+            // 기타 예외가 발생한 경우
+            return false;
+        }
     }
 
     public Member verifyEmail(String token) {
