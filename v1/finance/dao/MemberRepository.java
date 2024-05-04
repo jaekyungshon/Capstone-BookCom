35c73ae30b0b34428a0ac82d013b0cefbaa1c34b
kimwonjun1 <monkeydmw@naver.com>
2024-05-05 02:37:53 +0900

    Update MemberRepository.java

--- v1/finance/dao/MemberRepository.java
+++ v1/finance/dao/MemberRepository.java
@@ -1,13 +1,10 @@
-package com.capstone.finance.dao;
+package com.capstone.finance.DAO;
 
-import com.capstone.finance.model.Member;
-import jakarta.transaction.Transactional;
+import com.capstone.finance.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByUsername(String username);
     boolean existsByUsername(String username);
-    Optional<Member> findByNickname(String nickname);
 }
